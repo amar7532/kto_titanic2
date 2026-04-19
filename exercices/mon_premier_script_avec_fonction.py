5d5c6d7eae8336ecb3572d98ca66352c7d3d4286
amar7532 <amar.etu7@gmail.com>
2026-04-19 13:52:39 +0000

    feat:cleancode

--- exercices/mon_premier_script_avec_fonction.py
+++ exercices/mon_premier_script_avec_fonction.py
@@ -1,23 +1,19 @@
 import unittest
+from typing import List
 
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-        else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
+MIN_LENGTH = 7
 
-class TestNamesMethod(unittest.TestCase):
-     def test_names(self):
-        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
-        self.assertEqual(more_than_seven, 4)
 
-if __name__ == '__main__':
+def count_names_longer_than(names: List[str], min_length: int = MIN_LENGTH) -> int:
+    return sum(1 for name in names if len(name) > min_length)
+
+
+class TestCountNames(unittest.TestCase):
+    def test_count_names_longer_than(self):
+        names = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        result = count_names_longer_than(names)
+        self.assertEqual(result, 4)
+
+
+if __name__ == "__main__":
     unittest.main()
